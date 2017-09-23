b8ce9659bde73758cb38139c1ea20ea2966a752e
Paul Somers <paulsomers@gmail.com>
2017-09-23 17:19:40 +0100

    Initial PromiseTick.js

---
+++ PromiseTick.js
@@ -0,0 +1,30 @@
+class PromiseTick extends Promise {
+    constructor(fn) {
+        const tick = (...args) => this._tick ? this._tick(...args) : null;
+        const fnTicked = (res, rej) => fn(
+            (...args) => {
+                this._tick = null;
+                return res(...args);
+            },
+            (...args) => {
+                this._tick = null;
+                return rej(...args);
+            },
+            tick
+        );
+        super(fnTicked);
+    }
+    
+    tick(fn) {
+        this._tick = fn;
+        return this;
+    }
+    
+    then(res, rej, tick) {
+        this.tick(tick);
+        return super.then(res, rej);
+    }
+}
+
+export default PromiseTick;
+
